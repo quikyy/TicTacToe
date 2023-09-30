d2c20a553dcd3872c87f9427206adc1fd98ec724
quikyy <paczuskiszymon16@gmail.com>
2023-09-30 20:01:47 +0200

    computer know how to attack, take centre or corner field

--- src/main/java/org/tictactoe/Board.java
+++ src/main/java/org/tictactoe/Board.java
@@ -15,36 +15,39 @@ public class Board {
 
 	private List<Integer> allFields = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 	private List<Integer> availableFields = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
-	private Map<Integer, String> takenFields = new HashMap<>();
+	private List<Integer> cornerFields = List.of(1, 3, 7, 9);
+	private List<List<Integer>> oppositeCornerFields = List.of(List.of(1, 9), List.of(3, 7));
+
+	private Map<Integer, Player> takenFields = new HashMap<>();
 
 	public void drawBoard() {
 		System.out.println("");
 		System.out.println("Board:");
 		System.out.println();
 
-		String fieldNumber1 = takenFields.get(1) == null ? "-" : takenFields.get(1);
+		String fieldNumber1 = takenFields.get(1) == null ? "-" : takenFields.get(1).getSign();
 		drawField(fieldNumber1);
-		String fieldNumber2 = takenFields.get(2) == null ? "-" : takenFields.get(2);
+		String fieldNumber2 = takenFields.get(2) == null ? "-" : takenFields.get(2).getSign();
 		drawField(fieldNumber2);
-		String fieldNumber3 = takenFields.get(3) == null ? "-" : takenFields.get(3);
+		String fieldNumber3 = takenFields.get(3) == null ? "-" : takenFields.get(3).getSign();
 		drawField(fieldNumber3);
 
 		System.out.println();
 
-		String fieldNumber4 = takenFields.get(4) == null ? "-" : takenFields.get(4);
+		String fieldNumber4 = takenFields.get(4) == null ? "-" : takenFields.get(4).getSign();
 		drawField(fieldNumber4);
-		String fieldNumber5 = takenFields.get(5) == null ? "-" : takenFields.get(5);
+		String fieldNumber5 = takenFields.get(5) == null ? "-" : takenFields.get(5).getSign();
 		drawField(fieldNumber5);
-		String fieldNumber6 = takenFields.get(6) == null ? "-" : takenFields.get(6);
+		String fieldNumber6 = takenFields.get(6) == null ? "-" : takenFields.get(6).getSign();
 		drawField(fieldNumber6);
 
 		System.out.println();
 
-		String fieldNumber7 = takenFields.get(7) == null ? "-" : takenFields.get(7);
+		String fieldNumber7 = takenFields.get(7) == null ? "-" : takenFields.get(7).getSign();
 		drawField(fieldNumber7);
-		String fieldNumber8 = takenFields.get(8) == null ? "-" : takenFields.get(8);
+		String fieldNumber8 = takenFields.get(8) == null ? "-" : takenFields.get(8).getSign();
 		drawField(fieldNumber8);
-		String fieldNumber9 = takenFields.get(9) == null ? "-" : takenFields.get(9);
+		String fieldNumber9 = takenFields.get(9) == null ? "-" : takenFields.get(9).getSign();
 		drawField(fieldNumber9);
 
 		System.out.println();
@@ -54,7 +57,7 @@ public class Board {
 
 	public boolean takeField(int fieldId, Player player) {
 		if (availableFields.contains(fieldId)) {
-			takenFields.put(fieldId, player.getSign());
+			takenFields.put(fieldId, player);
 			player.getFieldsTaken().add(fieldId);
 			buildAvailableFields();
 			return true;
--- src/main/java/org/tictactoe/Game.java
+++ src/main/java/org/tictactoe/Game.java
@@ -2,8 +2,11 @@ package org.tictactoe;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import lombok.Getter;
 
 @Getter
@@ -25,11 +28,23 @@ public class Game {
 
 		while (!gameWon) {
 			for (Player player : players) {
+
+				if (board.getAvailableFields().size() == 0) {
+					System.out.println("It's a tie! Game ends.");
+					gameWon = true;
+					break;
+				}
+
 				board.drawBoard();
 				System.out.println("Player playing: " + player.getSign());
 				boolean isPlayerTookAvailableField = false;
 				if (player.isComputer()) {
 					isPlayerTookAvailableField = choiceFieldComputer();
+					gameWon = checkWin(player);
+					if (gameWon) {
+						System.out.println("Computer won the game!");
+						break;
+					}
 					continue;
 				}
 				while (!isPlayerTookAvailableField) {
@@ -50,6 +65,8 @@ public class Game {
 				}
 			}
 		}
+
+		board.drawBoard();
 	}
 
 
@@ -74,27 +91,122 @@ public class Game {
 			Player secondPlayer = new Player("O", new ArrayList<>(), false);
 			currPlayers.addAll(List.of(firstPlayer, secondPlayer));
 		}
-//		Collections.shuffle(currPlayers);
+		Collections.shuffle(currPlayers);
 		return currPlayers;
 	}
 
-	private boolean choiceFieldComputer() {
-		Player opponent = players.stream()
-				.filter(p -> !p.isComputer())
-				.findFirst()
-				.orElse(null);
 
+
+	private boolean choiceFieldComputer() {
 		Player computer = players.stream()
 				.filter(Player::isComputer)
 				.findFirst()
 				.orElse(null);
-		if (opponent == null || computer == null) {
+
+		Player opponent = players.stream()
+				.filter(player -> !player.isComputer())
+				.findFirst()
+				.orElse(null);
+
+		if (computer == null || opponent == null) {
 			return false;
 		}
 
-		List<Integer> opponentTakenFields = opponent.getFieldsTaken();
 		List<Integer> availableFields = board.getAvailableFields();
 		int computerChoice = 0;
+
+		// Computer moves as first
+		if (availableFields.size() == 9) {
+			System.out.println("Computer starts and computer will take corner field");
+			return board.takeField(choiceCornerField(), computer);
+		}
+
+		// Opponent moved first, check if he picked center field
+		if (availableFields.size() == 8) {
+			Player playerWhoTookCentreField = board.getTakenFields().get(5);
+			if (playerWhoTookCentreField == opponent) {
+				System.out.println("Player moved first and took cent");
+				return board.takeField(choiceCornerField(), computer);
+			}
+		}
+
+		// try to attack
+		computerChoice = attackMove(computer);
+		if (computerChoice != 0) {
+			return board.takeField(computerChoice, computer);
+		}
+
+		// try to defend
+		computerChoice = defendMove(computer, opponent);
+		if (computerChoice != 0) {
+			return board.takeField(computerChoice, computer);
+		}
+
+		computerChoice = checkOppositeCornerFields(computer, opponent);
+		if (computerChoice != 0 ) {
+			return board.takeField(computerChoice, computer);
+		}
+
+		if (computerChoice == 0) {
+			int randomInt = secureRandom.nextInt(availableFields.size());
+			computerChoice = availableFields.get(randomInt);
+			System.out.println("There was 0 matches in win combinations. Computer will pick random field");
+		}
+
+
+		System.out.println("Computer final choice: " + computerChoice);
+		return board.takeField(computerChoice, computer);
+	}
+
+	private int choiceCornerField() {
+		int randomInt = secureRandom.nextInt(board.getCornerFields().size());
+		return board.getCornerFields().get(randomInt);
+	}
+
+	private int attackMove(Player computer) {
+		List<Integer> computerTakenFields = computer.getFieldsTaken();
+		int computerChoice = 0;
+		for (List<Integer> winCombination : winsConditions) {
+			int matches = 0;
+			for (Integer fieldInWinCombination : winCombination) {
+				for (Integer computerTakenField : computerTakenFields) {
+					if (computerTakenField.equals(fieldInWinCombination)) {
+						matches++;
+					}
+				}
+			}
+			if (matches == 2) {
+				System.out.println("");
+				System.out.println("Computer should pick field from: " + winCombination + " to win the game");
+				System.out.println("");
+
+				computerChoice = winCombination.stream()
+						.filter(a -> !computerTakenFields.contains(a))
+						.findFirst()
+						.orElse(0);
+
+				System.out.println("Computer initial attack choice: " + computerChoice);
+
+				Player takenFieldPlayer = board.getTakenFields().get(computerChoice);
+
+				if (takenFieldPlayer == null) {
+					System.out.println("This field is free - computer will pick field: " + computerChoice);
+					break;
+				} else if (takenFieldPlayer != computer) {
+					System.out.println("Computer should pick " + computerChoice + " but is already taken by computer");
+					computerChoice = 0;
+					continue;
+				}
+				break;
+			}
+		}
+		System.out.println("Computer final attack choice: " + computerChoice);
+		return computerChoice;
+	}
+
+	private int defendMove(Player computer, Player opponent) {
+		int computerChoice = 0;
+		List<Integer> opponentTakenFields = opponent.getFieldsTaken();
 		for (List<Integer> winCombination : winsConditions) {
 			int matches = 0;
 			for (Integer fieldInWinCombination : winCombination) {
@@ -107,7 +219,7 @@ public class Game {
 			if (matches == 2) {
 
 				System.out.println("");
-				System.out.println("I should pick field from: " + winCombination + " to block from winning");
+				System.out.println("Computer should pick field from: " + winCombination + " to block from winning");
 				System.out.println("");
 
 				computerChoice = winCombination.stream()
@@ -115,14 +227,14 @@ public class Game {
 						.findFirst()
 						.orElse(0);
 
-				System.out.println("Computer initial choice: " + computerChoice);
+				System.out.println("Computer initial defend choice: " + computerChoice);
 
-				String computerChoiceFieldSign = board.getTakenFields().get(computerChoice);
+				Player takenFieldPlayer = board.getTakenFields().get(computerChoice);
 
-				if (computerChoiceFieldSign == null) {
+				if (takenFieldPlayer == null) {
 					System.out.println("This field is free - computer will pick field: " + computerChoice);
 					break;
-				} else if (computerChoiceFieldSign.equals(computer.getSign())) {
+				} else if (takenFieldPlayer == computer) {
 					System.out.println("Computer should pick " + computerChoice + " but is already taken by computer");
 					computerChoice = 0;
 					continue;
@@ -130,15 +242,44 @@ public class Game {
 				break;
 			}
 		}
-
-		if (computerChoice == 0) {
-			int randomInt = secureRandom.nextInt(availableFields.size());
-			computerChoice = availableFields.get(randomInt);
-			System.out.println("There was 0 matches in win combinations. Computer will pick random field");
-		}
-		System.out.println("Computer final choice: " + computerChoice);
-		return board.takeField(computerChoice, computer);
+		return computerChoice;
 	}
+
+	private int checkOppositeCornerFields(Player computer, Player opponent) {
+		int computerChoice = 0;
+		List<List<Integer>> oppositeCornerFields = board.getOppositeCornerFields();
+
+		for (List<Integer> cornerFields : oppositeCornerFields) {
+			for (Integer playerTakenField : opponent.getFieldsTaken()) {
+				if (cornerFields.contains(playerTakenField)) {
+					System.out.println("Opponent has corer field taken: " + playerTakenField + " computer will take opposite one");
+
+					computerChoice = cornerFields.stream()
+							.filter(a -> !opponent.getFieldsTaken().contains(a))
+							.findFirst()
+							.orElse(0);
+
+					System.out.println("Computer initial corner choice: " + computerChoice);
+
+					Player takenFieldPlayer = board.getTakenFields().get(computerChoice);
+
+					if (takenFieldPlayer == null) {
+						System.out.println("This field is free - computer will pick field: " + computerChoice);
+						break;
+					} else if (takenFieldPlayer == computer) {
+						System.out.println("Computer should pick " + computerChoice + " but is already taken by computer");
+						computerChoice = 0;
+						continue;
+					}
+					break;
+				}
+
+			}
+
+		}
+		return computerChoice;
+	}
+
 
 	private List<List<Integer>> buildWinsCombinations() {
 		List<Integer> winCombination1 = List.of(1, 2, 3);
